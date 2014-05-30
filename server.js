274bfac1fc442aea7348e7907dd84d2a3436a8bd
Alexander Wenzowski <pair+alex@partiallabs.com>
2014-05-29 21:55:18 -0400

    use forked derby-starter

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-var path = require('path')
-
-var app = require('./app')
+var path = require('path');
+var app = require('./app');
 var opts = {
-  port: process.env.PORT,
-  static: path.join(__dirname, 'node_modules/bootstrap/dist')
-}
+  static: [{
+    route: '/css',
+    dir: path.join(__dirname, 'node_modules/bootstrap/dist/css')
+  }]
+};
 
-require('derby-starter').run(app, opts)
+require('derby-starter').run(app, opts);
 
